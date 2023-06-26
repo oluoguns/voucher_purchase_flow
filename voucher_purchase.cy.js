b56e1de827bf5fb8c1da57c86ca3d5f1c39c99bd
oluoguns <131146696+oluoguns@users.noreply.github.com>
2023-06-26 11:31:32 +0100

    voucher_purchase.cy.js

--- voucher_purchase.cy.js
+++ voucher_purchase.cy.js
@@ -1,13 +1,14 @@
 describe("Voucher Purchase Flow", () => {
     it("Should successfully purchase a voucher", () => {
-      // Visit the demo salon page
+      
+        // Visit the demo salon page
       cy.visit("https://gift-cards.phorest.com/salons/demous#");
   
       // Select the $50 voucher option
       cy.get('input[data-voucher-value="50.00"]').click();
   
       // Enter email address
-      cy.get('input[data-target="email.purchaserEmailInput"]').type("johndoe.2023@gmail.com");
+      cy.get('input[data-target="email.purchaserEmailInput"]').type("sit-three@qlygy2tu.mailosaur.net");
   
       // Enter first name
       cy.get('input[data-target="name.purchaserFirstNameInput"]').type("John");
@@ -22,38 +23,53 @@ describe("Voucher Purchase Flow", () => {
       cy.contains('Value of gift card').next('p#confirm-voucher-value').should('contain', '$50.00');
   
       // Verify the sender's email address
-      cy.contains('Send receipt to').next('p#confirm-purchaser-email').should('contain', 'johndoe.2023@gmail.com');
+      cy.contains('Send receipt to').next('p#confirm-purchaser-email').should('contain', 'sit-three@qlygy2tu.mailosaur.net');
   
       // Verify the recipient's email address
-      cy.contains('Send gift card to').next('p#confirm-recipient-email').should('contain', 'johndoe.2023@gmail.com');
+      cy.contains('Send gift card to').next('p#confirm-recipient-email').should('contain', 'sit-three@qlygy2tu.mailosaur.net');
   
       cy.wait(3000);
   
       // Click on the "Confirm Details" button if everything matches up
       cy.get('button[data-action="confirm#confirmAction"]').click();
   
+      
       // Assert that the Checkout page is displayed
       cy.get('span[data-target="page.headerText"]').should('contain', 'Checkout');
   
       // Manually enter the payment details through the Cypress interface
       cy.log("Please enter the payment details manually in the Cypress interface");
-  
+
+      /* Payments Details
+         Name = John Doe
+         Zip code = 92606
+         Card Number = 4111 1111 1111 1111
+         Expiration = 12/23
+         Security Code = 999
+      */ 
+
       // Wait for the payment details to be entered manually
-      cy.pause();
+      cy.pause(15000);
+    
+      /* Once details have been entered and the Submit button has been clicked, manually resume 
+         the test to by clicking on the 'Resume' button to continue running the test 
+      */
+      
+
   // Assert that the "Purchase Complete" message is displayed
   cy.get('body > div.flex.flex-col.h-full > header > span').should('contain', 'Purchase Complete');
 
-  // Assert that the payment success message is displayed
-  cy.get('body > div.flex.flex-col.h-full > div > div > div.container.my-4.text-center > p.text-xl.font-medium.mb-8')
-    .should('contain', 'Payment accepted, thank you!');
 
-  // Assert that the gift card and receipt sent message is displayed
-  cy.contains('Your gift card has been sent. We\'ve also sent you a receipt.');
+    cy.wait(3000);
 
   // Click the "Done" button
   cy.get('body > div.flex.flex-col.h-full > div > div > div.container.mb-10 > button').click();
-  
+
       // You can add more assertions or verifications here if needed
+    
+      cy.end();
+    
     });
   });
+
   
